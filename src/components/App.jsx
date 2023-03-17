2a382df859ecd9c0e876ead1e6154e0dbd3fb4f1
YuriiKabo <yourrocksmirnov@gmail.com>
2023-03-17 14:55:24 +0200

    current

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,76 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import ContactForm from './ContactForm/ContactForm';
+// import PropTypes from 'prop-types';
+import { nanoid } from 'nanoid';
+
+export class App extends Component {
+  state = {
+    text: '',
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+  };
+
+  formSubmitHandler = data => {
+    data.id = nanoid();
+    this.setState({ contacts: [data, ...this.state.contacts] });
+  };
+
+  handleDelete = id => {
+    this.setState({
+      contacts: this.state.contacts.filter(contact => contact.id !== id),
+    });
+  };
+
+  handleSearchInputChange = e => {
+    const { name, value } = e.currentTarget;
+    this.setState({ [name]: value });
+    this.filterSearch();
+  };
+
+  filterSearch = () => {
+    this.setState({
+      contacts: this.state.contacts.filter(contact =>
+        contact.name.toLowerCase().includes(this.state.text.toLowerCase())
+      ),
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={this.formSubmitHandler} />
+        <h2>Contacts</h2>
+        <div>
+          <label>
+            <p>Find contacts by name</p>
+            <input
+              type="tel"
+              name="text"
+              required
+              value={this.state.text}
+              onChange={this.handleSearchInputChange}
+            />
+          </label>
+        </div>
+        <div>
+          <ul>
+            {this.state.contacts.map(({ id, name, number }) => (
+              <li key={id}>
+                <span>{name}: </span>
+                <span>{number}</span>
+                <button type="button" onClick={() => this.handleDelete(id)}>
+                  Delete
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,56 @@
+import { Component } from 'react';
+
+class ContactForm extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  handleInputChange = e => {
+    const { name, value } = e.currentTarget;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.onSubmit(this.state);
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({ name: '', number: '' });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          <p>Name</p>
+          <input
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            value={this.state.name}
+            onChange={this.handleInputChange}
+            required
+          />
+        </label>
+        <label>
+          <p>Number</p>
+          <input
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            value={this.state.number}
+            onChange={this.handleInputChange}
+            required
+          />
+          <button type="submit">Add contact</button>
+        </label>
+      </form>
+    );
+  }
+}
+export default ContactForm;
